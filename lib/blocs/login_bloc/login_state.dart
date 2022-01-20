8dc91d6bb066ddef6533e20364dedaf2b15b5b62
78Ronin78 <superdiman_1994@mail.ru>
2022-01-20 22:45:36 +0800

    Add color state field
    Add valid color for check formfalidate

--- lib/blocs/login_bloc/login_state.dart
+++ lib/blocs/login_bloc/login_state.dart
@@ -6,6 +6,7 @@ class LoginState {
   final bool isSubmitting;
   final bool isSuccess;
   final bool isFailure;
+  final Color validColor;
 
   bool get isFormValid => isEmailValid && isPasswordValid;
 
@@ -14,46 +15,47 @@ class LoginState {
       this.isPasswordValid,
       this.isSubmitting,
       this.isSuccess,
-      this.isFailure});
+      this.isFailure,
+      this.validColor});
 
   factory LoginState.initial() {
     return LoginState(
-      isEmailValid: true,
-      isPasswordValid: true,
-      isSubmitting: false,
-      isSuccess: false,
-      isFailure: false,
-    );
+        isEmailValid: true,
+        isPasswordValid: true,
+        isSubmitting: false,
+        isSuccess: false,
+        isFailure: false,
+        validColor: Colors.white);
   }
 
   factory LoginState.loading() {
     return LoginState(
-      isEmailValid: true,
-      isPasswordValid: true,
-      isSubmitting: true,
-      isSuccess: false,
-      isFailure: false,
-    );
+        isEmailValid: true,
+        isPasswordValid: true,
+        isSubmitting: true,
+        isSuccess: false,
+        isFailure: false,
+        validColor: Color(0xFFADFF2F));
   }
 
   factory LoginState.failure() {
     return LoginState(
-      isEmailValid: true,
-      isPasswordValid: true,
-      isSubmitting: false,
-      isSuccess: false,
-      isFailure: true,
-    );
+        isEmailValid: true,
+        isPasswordValid: true,
+        isSubmitting: false,
+        isSuccess: false,
+        isFailure: true,
+        validColor: Colors.red);
   }
 
   factory LoginState.success() {
     return LoginState(
-      isEmailValid: true,
-      isPasswordValid: true,
-      isSubmitting: false,
-      isSuccess: true,
-      isFailure: false,
-    );
+        isEmailValid: true,
+        isPasswordValid: true,
+        isSubmitting: false,
+        isSuccess: true,
+        isFailure: false,
+        validColor: Color(0xFFADFF2F));
   }
 
   LoginState update({
@@ -61,27 +63,27 @@ class LoginState {
     bool isPasswordValid,
   }) {
     return copyWith(
-      isEmailValid: isEmailValid,
-      isPasswordValid: isPasswordValid,
-      isSubmitting: false,
-      isSuccess: false,
-      isFailure: false,
-    );
+        isEmailValid: isEmailValid,
+        isPasswordValid: isPasswordValid,
+        isSubmitting: false,
+        isSuccess: false,
+        isFailure: false,
+        validColor: Colors.white);
   }
 
-  LoginState copyWith({
-    bool isEmailValid,
-    bool isPasswordValid,
-    bool isSubmitting,
-    bool isSuccess,
-    bool isFailure,
-  }) {
+  LoginState copyWith(
+      {bool isEmailValid,
+      bool isPasswordValid,
+      bool isSubmitting,
+      bool isSuccess,
+      bool isFailure,
+      Color validColor}) {
     return LoginState(
-      isEmailValid: isEmailValid ?? this.isEmailValid,
-      isPasswordValid: isPasswordValid ?? this.isPasswordValid,
-      isSubmitting: isSubmitting ?? this.isSubmitting,
-      isSuccess: isSuccess ?? this.isSuccess,
-      isFailure: isFailure ?? this.isFailure,
-    );
+        isEmailValid: isEmailValid ?? this.isEmailValid,
+        isPasswordValid: isPasswordValid ?? this.isPasswordValid,
+        isSubmitting: isSubmitting ?? this.isSubmitting,
+        isSuccess: isSuccess ?? this.isSuccess,
+        isFailure: isFailure ?? this.isFailure,
+        validColor: validColor ?? this.validColor);
   }
 }
