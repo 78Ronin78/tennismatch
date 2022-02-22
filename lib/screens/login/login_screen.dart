be60ef19954fe1bbe360d85338aad2e3c5f963ef
78Ronin78 <superdiman_1994@mail.ru>
2022-02-22 12:43:34 +0800

    Убрал виджеты формы с экрана

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tennis_match_app/blocs/login_bloc/login_bloc.dart';
-import 'package:tennis_match_app/screens/register/register_screen.dart';
-import 'package:tennis_match_app/widgets/text_widget.dart';
 
 import 'login_form.dart';
 
@@ -25,14 +23,8 @@ class LoginScreen extends StatelessWidget {
               SizedBox(height: 75),
               SvgPicture.asset(
                 'assets/images/logotype_yellow.svg',
-                height: 200,
+                height: 150,
               ),
-              // Image.asset(
-              //   'assets/images/login.png',
-              //   width: double.infinity,
-              //   height: 170,
-              //   fit: BoxFit.contain,
-              // ),
               SizedBox(height: 25),
               Container(
                 width: double.infinity,
@@ -46,7 +38,6 @@ class LoginScreen extends StatelessWidget {
                   ),
                 ),
               ),
-
               Container(
                 width: double.infinity,
                 padding: EdgeInsets.symmetric(horizontal: 35, vertical: 5),
@@ -60,72 +51,9 @@ class LoginScreen extends StatelessWidget {
                 ),
               ),
               LoginForm(),
-
-              Padding(
-                padding: const EdgeInsets.all(5),
-                child: Text(
-                  'Забыли пароль ?',
-                  style: TextStyle(
-                    color: Color(0xFFADFF2F),
-                  ),
-                ),
-              ),
-              SizedBox(height: 45),
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => RegisterScreen(),
-                    ),
-                  );
-                },
-                child: Text_Widget(
-                    text: 'Нет учетной записи ?', signText: 'СОЗДАТЬ'),
-              ),
             ],
           )),
         ),
-
-        /*Container(
-          height: double.infinity,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xfff2cbd0), Color(0xfff4ced9)],
-          )),
-          child: SingleChildScrollView(
-            child: Stack(
-              children: <Widget>[
-                CurvedWidget(
-                  child: Container(
-                    padding: const EdgeInsets.only(top: 100, left: 50),
-                    width: double.infinity,
-                    height: 300,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [Colors.white, Colors.white.withOpacity(0.4)],
-                      ),
-                    ),
-                    child: Text(
-                      'Вход',
-                      style: TextStyle(
-                        fontSize: 40,
-                        color: Color(0xff6a515e),
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 230),
-                  child: LoginForm(),
-                )
-              ],
-            ),
-          ),
-        ),*/
       ),
     );
   }
