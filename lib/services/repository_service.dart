f5f1c7347e1e9b649290ec58df0e4021e049bffa
78Ronin78 <superdiman_1994@mail.ru>
2022-02-28 14:42:01 +0800

    Добавил перенаправление после авторизации пользователя в соц.сети

--- lib/services/repository_service.dart
+++ lib/services/repository_service.dart
@@ -1,7 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tennis_match_app/blocs/home_bloc/home_bloc.dart';
 import 'package:tennis_match_app/models/chat_message.dart';
 import 'package:tennis_match_app/models/chat_room_info.dart';
 import 'package:tennis_match_app/models/user.dart';
+import 'package:tennis_match_app/screens/home_screen.dart';
+import 'package:tennis_match_app/screens/register/register_screen.dart';
 
 class RepositoryService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -27,6 +32,38 @@ class RepositoryService {
       'name': user.name,
       'imgUrl': user.imgUrl
     });
+  }
+
+  //Функция перенаправления пользователя, если он авторизовался через google
+  Future<UserProfile> redirectAuthUser(BuildContext context, String uid) async {
+    DocumentSnapshot documentSnapshot =
+        await _firestore.collection('users').doc(uid).get();
+    var user;
+    if (documentSnapshot != null && documentSnapshot.exists) {
+      user = UserProfile.fromJson(documentSnapshot.data());
+      print('ID: ${user.id}');
+      if (user.name != null) {
+        //Navigator.pushNamed(context, 'tabNavigator');
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(
+                builder: (context) => BlocProvider(
+                      create: (context) => HomeBloc(),
+                      child: HomeScreen(),
+                    )),
+            (Route<dynamic> route) => false);
+      } else {
+        //Navigator.pushNamed(context, 'registrationScreen');
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => RegisterScreen()),
+            (Route<dynamic> route) => false);
+      }
+    } else {
+      //Navigator.pushNamed(context, 'registrationScreen');
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) => RegisterScreen()),
+          (Route<dynamic> route) => false);
+    }
+    return null;
   }
 
   Future<void> setChatRoom(ChatRoomInfo chatRoomInfo) async {
