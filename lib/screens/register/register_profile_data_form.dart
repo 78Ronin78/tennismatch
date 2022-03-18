1dae25fc1ce78a47303357ae0609fc82a4aaeb37
78Ronin78 <superdiman_1994@mail.ru>
2022-03-18 09:28:31 +0800

    Создал файл для создания виджета формы анкеты профиля пользователя

---
+++ lib/screens/register/register_profile_data_form.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RegisterProfileDateForm extends StatefulWidget {
+  const RegisterProfileDateForm({Key key}) : super(key: key);
+
+  @override
+  State<RegisterProfileDateForm> createState() =>
+      _RegisterProfileDateFormState();
+}
+
+class _RegisterProfileDateFormState extends State<RegisterProfileDateForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
